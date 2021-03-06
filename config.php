a7fb9f8d32c89d8603b447c5f11309badccaf4e7
bipinparajuli <bipinprjl@gmail.com>
2021-03-06 21:19:52 +0545

    read operation added

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$con = mysqli_connect("localhost","root","","student");
+
+if(!$con)
+{
+    echo "connection faliled",mysqli_error();
+}
+
+?>
---
+++ read.php
@@ -0,0 +1,81 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+require_once "config.php";
+
+$sql = "SELECT * FROM bca";
+
+$result = mysqli_query($con,$sql);
+
+if(mysqli_num_rows($result) > 0){
+    echo " <Table cellspaning=0 border=1>";
+    echo "<Thead>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>Name</th>";
+    echo "<th>Phone</th>";
+    echo "<th>Address</th>";
+    echo "<th>Email</th>";
+    
+    echo "</tr>";
+    
+    echo "</Thead>";
+    echo "<tbody>";
+    echo "<tr>";    
+while($row = mysqli_fetch_assoc($result))
+{
+$id=$row["id"];
+$name=$row["name"];
+$phone=$row["phone"];
+$address=$row["address"];
+$email=$row["email"];
+
+
+
+echo "<td>".$id."</td>";
+echo "<td>".$name."</td>";
+echo "<td>".$phone."</td>";
+echo "<td>".$address."</td>";
+echo "<td>".$email."</td>";
+
+echo "</tr>";
+
+
+}
+echo "</tbody>";
+
+echo"</Table>";
+}
+else{
+    echo "Result 0";
+}
+
+mysqli_close($con);
+?>
+
+
+    <tr>
+    <th>Id</th>
+    <th>Name</th>
+    <th>Phone</th>
+    <th>Address</th>
+    <th>Email</th>
+    </tr>
+    <tr>
+    <td>$id</td>
+    <td>$name</td>
+    <td>$phone</td>
+    <td>$address</td>
+    <td>$email</td>
+    </tr>
+    </table>
+
+    </body>
+</html>
